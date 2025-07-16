3304829c852006ab1836c4486789fbf1aecf33a7
papapaul-36 <jongyeon.lee.sda@gmail.com>
2025-07-16 15:21:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,15 @@ model = load_model()  # ← 반드시 함수 밖에 있어야 정상 작동함!
 st.set_page_config(layout="wide")
 st.title("📚 생기부 문장 유사도 검사기")
 
+with open("example.xlsx", "rb") as f:s
+    st.download_button(
+        label="📥 예시 엑셀 파일 다운로드",
+        data=f,
+        file_name="example.xlsx",
+        mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
+
+
 uploaded_file = st.file_uploader("📎 엑셀 파일 업로드 (학생 이름, 세특 전체)", type="xlsx")
 
 # ✅ 공통 단어 하이라이트 함수
